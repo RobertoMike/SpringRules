a3e77809ef418286c0e40465ecd99d7fca685192
Giorgio-work <giorgio.work24@gmail.com>
2024-12-05 21:21:04 +0100

    FIx: prop message in PasswordValidation

--- src/main/kotlin/io/github/robertomike/springrules/validations/PasswordValidation.kt
+++ src/main/kotlin/io/github/robertomike/springrules/validations/PasswordValidation.kt
@@ -3,7 +3,6 @@ package io.github.robertomike.springrules.validations
 import io.github.robertomike.springrules.constraints.PasswordConstraint
 import jakarta.validation.Constraint
 import jakarta.validation.Payload
-import java.lang.annotation.ElementType
 import kotlin.reflect.KClass
 
 
@@ -12,10 +11,7 @@ import kotlin.reflect.KClass
 @Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class PasswordValidation(
-    val message: String = ("It needs to contain 1 uppercase letter, " +
-            "1 lowercase letter, " +
-            "1 digit, " +
-            "1 special character and be 8 characters long."),
+    val message: String = "{spring-rules.password}",
     val canBeEmpty: Boolean = false,
     val groups: Array<KClass<*>> = [],
     val payload: Array<KClass<out Payload>> = []
