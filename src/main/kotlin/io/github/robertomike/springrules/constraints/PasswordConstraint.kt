cd4c41d463bed47be1c974f4518c2f544df82adb
Roberto Micheletti <rmworking@hotmail.com>
2024-12-06 10:54:26 +0100

    Feat: improving the PasswordValidation

--- src/main/kotlin/io/github/robertomike/springrules/constraints/PasswordConstraint.kt
+++ src/main/kotlin/io/github/robertomike/springrules/constraints/PasswordConstraint.kt
@@ -1,15 +1,47 @@
 package io.github.robertomike.springrules.constraints
 
+import io.github.robertomike.springrules.utils.MessageUtil
 import io.github.robertomike.springrules.validations.PasswordValidation
 
-class PasswordConstraint: SimpleConstraint<PasswordValidation, String>() {
-    override fun isValid(password: String): Boolean {
-        /* Password needs to be as long as required, contains at least one digit,
-         one lowercase letter, one uppercase letter and one special character */
-        return password.length >= 8
-                && password.matches("(?=.*\\d).*".toRegex())
-                && password.matches("(?=.*[a-z]).*".toRegex())
-                && password.matches("(?=.*[A-Z]).*".toRegex())
-                && password.matches("(?=.*[~!@#$%^&*()_-]).*".toRegex())
+class PasswordConstraint: SimpleMessageConstraint<PasswordValidation, String>() {
+    private val symbolRegex = "(?=.*[~!@#$%^&*()_-]).*".toRegex()
+    private val lowercaseRegex = "(?=.*[a-z]).*".toRegex()
+    private val uppercaseRegex = "(?=.*[A-Z]).*".toRegex()
+    private val letterRegex = ".*[a-zA-Z].*".toRegex()
+    private val numberRegex = "(?=.*\\d).*".toRegex()
+
+    override fun isValid(value: String, util: MessageUtil): Boolean {
+        val messages = mutableListOf<String>()
+
+        if (value.length < annotation.minLength) {
+            messages.add("password.length.min")
+        }
+
+        if (value.length > annotation.maxLength) {
+            messages.add("password.length.max")
+        }
+
+        if (annotation.specialCharacters && !value.matches(symbolRegex)) {
+            messages.add("password.special-character")
+        }
+
+        if (annotation.number && !value.matches(numberRegex)) {
+            messages.add("password.number")
+        }
+
+        if (annotation.uppercaseAndLowercase && !(value.matches(lowercaseRegex) && value.matches(uppercaseRegex))) {
+            messages.add("password.number")
+        }
+
+        if (annotation.letters && !value.matches(letterRegex)) {
+            messages.add("password.letter")
+        }
+
+        if (messages.isNotEmpty()) {
+            util.changeMessages(*messages.toTypedArray())
+            return false
+        }
+
+        return true
     }
 }
--- src/main/kotlin/io/github/robertomike/springrules/validations/PasswordValidation.kt
+++ src/main/kotlin/io/github/robertomike/springrules/validations/PasswordValidation.kt
@@ -5,13 +5,18 @@ import jakarta.validation.Constraint
 import jakarta.validation.Payload
 import kotlin.reflect.KClass
 
-
 @MustBeDocumented
 @Constraint(validatedBy = [PasswordConstraint::class])
 @Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class PasswordValidation(
-    val message: String = "{spring-rules.password}",
+    val message: String = "{spring-rules.password.default}",
+    val minLength: Int = 8,
+    val maxLength: Int = 256,
+    val number: Boolean = true,
+    val letters: Boolean = true,
+    val uppercaseAndLowercase: Boolean = true,
+    val specialCharacters: Boolean = true,
     val groups: Array<KClass<*>> = [],
     val payload: Array<KClass<out Payload>> = []
 )
