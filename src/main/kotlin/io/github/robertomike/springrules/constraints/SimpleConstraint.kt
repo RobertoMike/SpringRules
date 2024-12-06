a9b285f61049c8ae826656ed2fc926310ba7dade
Roberto Micheletti <rmworking@hotmail.com>
2024-12-06 08:12:24 +0100

    Feat: transforming interface into class with initialize method, added try-catch block with custom messages

--- src/main/kotlin/io/github/robertomike/springrules/constraints/SimpleConstraint.kt
+++ src/main/kotlin/io/github/robertomike/springrules/constraints/SimpleConstraint.kt
@@ -1,17 +1,31 @@
 package io.github.robertomike.springrules.constraints
 
+import io.github.robertomike.springrules.exceptions.SpringRulesException
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
+import java.lang.reflect.InvocationTargetException
 
 @FunctionalInterface
-interface SimpleConstraint<A : Annotation, T> : ConstraintValidator<A, T> {
+abstract class SimpleConstraint<A: Annotation, T>: ConstraintValidator<A, T> {
+    protected lateinit var annotation: A
+
+    override fun initialize(constrain: A) {
+        annotation = constrain
+    }
+
     override fun isValid(value: T?, context: ConstraintValidatorContext): Boolean {
         if (value == null) {
             return true
         }
 
-        return isValid(value)
+        try {
+            return isValid(value)
+        } catch (e: NoSuchMethodException) {
+            throw SpringRulesException("There was an error retrieving the method", e)
+        } catch (e: InvocationTargetException) {
+            throw SpringRulesException("There was an error invoking some method", e)
+        }
     }
 
-    fun isValid(value: T): Boolean
+    abstract fun isValid(value: T): Boolean
 }
