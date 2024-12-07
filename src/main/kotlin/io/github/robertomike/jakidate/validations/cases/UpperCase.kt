a77e5941505d860effbaecc2e4fd7d999ebae068
Giorgio-work <giorgio.work24@gmail.com>
2024-12-07 14:49:23 +0100

    Feat: added new class for uppercase validation

---
+++ src/main/kotlin/io/github/robertomike/jakidate/validations/cases/UpperCase.kt
@@ -0,0 +1,16 @@
+package io.github.robertomike.jakidate.validations.cases
+
+import jakarta.validation.constraints.Pattern
+
+@MustBeDocumented
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.TYPE
+)
+@Retention(AnnotationRetention.RUNTIME)
+@Pattern(regexp = "^([a-z])+\$", message = "{spring-rules.case.snake}")
+annotation class UpperCase
