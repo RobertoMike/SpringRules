1d851edf36f2ac0a7f8060f3de64770914082358
Giorgio-work <giorgio.work24@gmail.com>
2024-12-07 17:10:52 +0100

    Feat: added between date and datetime

--- src/main/kotlin/io/github/robertomike/jakidate/constraints/BetweenConstraint.kt
+++ src/main/kotlin/io/github/robertomike/jakidate/constraints/BetweenConstraint.kt
@@ -1,6 +1,10 @@
 package io.github.robertomike.jakidate.constraints
 
+import io.github.robertomike.jakidate.utils.toDate
+import io.github.robertomike.jakidate.utils.toLocalDateTime
 import io.github.robertomike.jakidate.validations.Between
+import java.time.LocalDateTime
+import java.util.Date
 
 class BetweenConstraint: SimpleConstraint<Between, Any>() {
 
@@ -9,6 +13,8 @@ class BetweenConstraint: SimpleConstraint<Between, Any>() {
             is Int, is Long -> value in annotation.min.toLong()..annotation.max.toLong()
             is Double -> value in annotation.min.toDouble()..annotation.max.toDouble()
             is Float -> value in annotation.min.toFloat()..annotation.max.toFloat()
+            is Date -> value in annotation.min.toDate("YYYY-MM-DD")..annotation.max.toDate("YYYY-MM-DD")
+            is LocalDateTime -> value in annotation.min.toLocalDateTime()..annotation.max.toLocalDateTime()
             else -> throw UnsupportedOperationException("Type ${value::class.simpleName} is not supported")
         }
     }
