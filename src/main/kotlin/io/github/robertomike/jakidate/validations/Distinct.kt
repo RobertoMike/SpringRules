d8b862c05139b0d16a96b25583afa5235e6cf73b
Roberto Micheletti <rmworking@hotmail.com>
2024-12-10 13:54:15 +0100

    Code smell

--- src/main/kotlin/io/github/robertomike/jakidate/validations/Distinct.kt
+++ src/main/kotlin/io/github/robertomike/jakidate/validations/Distinct.kt
@@ -1,7 +1,6 @@
 package io.github.robertomike.jakidate.validations
 
 import io.github.robertomike.jakidate.constraints.DistinctConstraint
-import io.github.robertomike.jakidate.constraints.required.RequiredIfConstraint
 import jakarta.validation.Constraint
 import jakarta.validation.Payload
 import kotlin.reflect.KClass
