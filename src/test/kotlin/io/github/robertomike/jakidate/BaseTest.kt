cbcbfcadc7a206e652343ef4ecd2fd0c7420ec09
Roberto Micheletti <rmworking@hotmail.com>
2024-12-12 19:24:35 +0100

    Feat: adding utility method test

--- src/test/kotlin/io/github/robertomike/jakidate/BaseTest.kt
+++ src/test/kotlin/io/github/robertomike/jakidate/BaseTest.kt
@@ -1,4 +1,18 @@
 package io.github.robertomike.jakidate
 
+import io.github.robertomike.jakidate.configs.CustomTest
+import org.yaml.snakeyaml.Yaml
+import java.io.FileInputStream
+
 @CustomTest
-abstract class BaseTest
+abstract class BaseTest {
+    companion object {
+        fun loadAndGetYaml(fileName: String): Map<String, List<String>> {
+            val file = BaseTest::class.java.getResource("$fileName.yaml")?.file
+                ?: throw RuntimeException("File $fileName not found, example stringsEan == /strings/en.yml")
+
+            val yaml = Yaml()
+            return yaml.load(FileInputStream(file))
+        }
+    }
+}
