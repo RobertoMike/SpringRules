1423295d6dd7813bac68958c15e882e882a77460
Roberto Micheletti <rmworking@hotmail.com>
2024-12-26 21:38:55 +0100

    Code smell

--- SpringRules/src/main/kotlin/io/github/robertomike/springrules/constraints/ExtensionConstraint.kt
+++ SpringRules/src/main/kotlin/io/github/robertomike/springrules/constraints/ExtensionConstraint.kt
@@ -3,7 +3,6 @@ package io.github.robertomike.springrules.constraints
 import io.github.robertomike.jakidate.constraints.SimpleConstraint
 import io.github.robertomike.springrules.validations.Extension
 import org.springframework.web.multipart.MultipartFile
-import java.util.*
 
 class ExtensionConstraint: SimpleConstraint<Extension, MultipartFile>() {
     override fun isValid(value: MultipartFile): Boolean {
