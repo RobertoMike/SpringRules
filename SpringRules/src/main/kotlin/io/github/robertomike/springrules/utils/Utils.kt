ae388a0be5feb22f8207f78bca7abf8e9b5f69e2
Roberto Micheletti <rmworking@hotmail.com>
2024-12-26 21:36:56 +0100

    Fix: in case the method is declared with generic, find it only by name

--- SpringRules/src/main/kotlin/io/github/robertomike/springrules/utils/Utils.kt
+++ SpringRules/src/main/kotlin/io/github/robertomike/springrules/utils/Utils.kt
@@ -1,13 +1,14 @@
 package io.github.robertomike.springrules.utils
 
 import org.springframework.context.ApplicationContext
-import org.springframework.util.ClassUtils.getMethod
+import org.springframework.util.ClassUtils.*
 import java.lang.reflect.Method
 import java.util.*
 
 fun ApplicationContext.getBeanAndExecute(repository: Class<out Any>, method: String, value: Any): Any? {
     val instance = this.getBean(repository)
-    val callable: Method = getMethod(repository, method, value.javaClass)
+    val callable: Method = getMethodIfAvailable(repository, method, value.javaClass)
+        ?: getMethod(repository, method, null)
     return callable.invoke(instance, value)
 }
 
