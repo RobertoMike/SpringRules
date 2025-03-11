20b34319abf8f1bc4cc97df297d6bb19846d4b58
Roberto Micheletti <rmworking@hotmail.com>
2025-03-11 22:33:59 +0100

    Feat: adding test for rgb

---
+++ src/test/kotlin/io/github/robertomike/jakidate/validations/colors/RgbTest.kt
@@ -0,0 +1,43 @@
+package io.github.robertomike.jakidate.validations.colors
+
+import io.github.robertomike.jakidate.enums.RgbOptions
+import io.github.robertomike.jakidate.utils.YamlFileSource
+import io.github.robertomike.jakidate.utils.YamlSource
+import io.github.robertomike.jakidate.validations.YamlTest
+import jakarta.validation.Validator
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+
+@YamlFileSource("/colors/rgb")
+class RgbTest : YamlTest() {
+    inner class Example(
+        @field:RgbColor
+        val value: String
+    )
+    inner class Legacy(
+        @field:RgbColor(RgbOptions.LEGACY)
+        val value: String
+    )
+    inner class New(
+        @field:RgbColor(RgbOptions.NEW)
+        val value: String
+    )
+
+    override fun getExample(value: String): Any {
+        return Example(value)
+    }
+
+    @Test
+    fun goodLegacy(validator: Validator) {
+        val constraints = validator.validate(Legacy("rgb(255,255,255)"))
+
+        assert(constraints.isEmpty())
+    }
+
+    @Test
+    fun goodNew(validator: Validator) {
+        val constraints = validator.validate(New("rgb(255 255 255)"))
+
+        assert(constraints.isEmpty())
+    }
+}
