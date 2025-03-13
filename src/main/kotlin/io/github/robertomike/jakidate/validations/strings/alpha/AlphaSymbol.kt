98957df3a1aa13e54c4ca628244658a4a9494ca7
Giorgio-work <giorgio.work24@gmail.com>
2025-03-13 22:50:11 +0100

    feat: alphaSymbol annotation modified
    (cherry picked from commit 3e29582f848cbb596fd75623e8743f312b849c92)

--- src/main/kotlin/io/github/robertomike/jakidate/validations/strings/alpha/AlphaSymbol.kt
+++ src/main/kotlin/io/github/robertomike/jakidate/validations/strings/alpha/AlphaSymbol.kt
@@ -7,7 +7,7 @@ import javax.validation.constraints.Pattern
 import kotlin.reflect.KClass
 
 /**
- * The string must only contain symbols or alpha symbols
+ * The string must only contain symbols, numbers or alpha symbols
  *
  * @author Giorgio Andrei
  * @since 1.0.0
@@ -25,7 +25,7 @@ import kotlin.reflect.KClass
 @Repeatable
 @Constraint(validatedBy = [])
 @ReportAsSingleViolation
-@Pattern(regexp = "^([.,;:?!'\"\\-—_/\\\\()\\[\\]{}@#&%*+=<>≠≈√Δ∑π¢$€£¥|~^¿¡ÁÉÍÓÚÀÈÌÒÙÄËÏÖÜÇØÆßa-zA-Z0-9]+)$", message = "{jakidate.strings.alpha.symbols}")
+@Pattern(regexp = "^([.,;:?!'\"\\-—_/\\\\()\\[\\]{}@#&%*+=<>≠≈√Δ∑π¢\$€£¥|~^¿¡ÁÉÍÓÚÀÈÌÒÙÄËÏÖÜÇØÆßa-zA-Z]+)\$", message = "{jakidate.strings.alpha.symbols}")
 annotation class AlphaSymbol(
     /**
      * the error message template
