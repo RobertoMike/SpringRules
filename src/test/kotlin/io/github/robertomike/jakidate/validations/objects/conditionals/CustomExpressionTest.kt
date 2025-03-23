e74f9313b3a892ca518747b9942487e2e02f6f5c
Roberto Micheletti <rmworking@hotmail.com>
2025-03-23 14:25:21 +0100

    Feat: updating test

---
+++ src/test/kotlin/io/github/robertomike/jakidate/validations/objects/conditionals/CustomExpressionTest.kt
@@ -0,0 +1,65 @@
+package io.github.robertomike.jakidate.validations.objects.conditionals
+
+import io.github.robertomike.jakidate.BaseTest
+import jakarta.validation.Validator
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CustomExpressionTest : BaseTest() {
+    enum class PersonTypeEnum {
+        NATURAL, LEGAL_PERSON
+    }
+
+    @Exclude
+    @Required
+    inner class Example(
+        @field:Conditional(expression = CustomExpression::class)
+        val personType: PersonTypeEnum,
+        @field:ExcludeIf // control != PersonTypeEnum.NATURAL
+        @field:RequiredUnless
+        val fiscalCode: String?,
+        @field:ExcludeUnless // control != PersonTypeEnum.LEGAL_PERSON
+        @field:RequiredIf
+        val pIva: String?
+    )
+
+    @Test
+    fun good(validator: Validator) {
+        val example = Example(
+            PersonTypeEnum.NATURAL,
+            "Ciao",
+            ""
+        )
+
+        val constraints = validator.validate(example)
+
+        assert(constraints.isEmpty())
+    }
+
+    @Test
+    fun goodLegal(validator: Validator) {
+        val example = Example(
+            PersonTypeEnum.LEGAL_PERSON,
+            "",
+            "Ciao"
+        )
+
+        val constraints = validator.validate(example)
+
+        assert(constraints.isEmpty())
+    }
+
+    @Test
+    fun error(validator: Validator) {
+        val example = Example(
+            PersonTypeEnum.LEGAL_PERSON,
+            "Ciao",
+            "Ciao"
+        )
+
+        val constraints = validator.validate(example)
+
+        assertEquals(1, constraints.size)
+        assertEquals("fiscalCode", constraints.first().propertyPath.first().name)
+    }
+}
