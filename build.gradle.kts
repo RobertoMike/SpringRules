cb546abcee679057c4e663316e03aaee095dc83e
Roberto Micheletti <rmworking@hotmail.com>
2025-03-24 20:37:40 +0100

    Feat: adding a SLF4J to see the messages

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     testImplementation("org.yaml:snakeyaml:2.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.slf4j:slf4j-api:1.7.36")
+    testImplementation("ch.qos.logback:logback-classic:1.2.11")
 }
 
 // Specifies the build version for Java
