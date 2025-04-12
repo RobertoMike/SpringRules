9ba23e1f462271bf25ec37694505794f013e4f9c
Roberto Micheletti <rmworking@hotmail.com>
2025-04-12 21:38:45 +0200

    Fix: trying to solve a problem with spring bean resolution

--- SpringRules/src/main/kotlin/io/github/robertomike/springrules/utils/Utils.kt
+++ SpringRules/src/main/kotlin/io/github/robertomike/springrules/utils/Utils.kt
@@ -1,9 +1,35 @@
 package io.github.robertomike.springrules.utils
 
 import org.springframework.context.ApplicationContext
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
 import org.springframework.util.ClassUtils.getMethodIfAvailable
+import org.springframework.util.StringUtils
 import java.lang.reflect.Method
 import java.util.*
+
+fun ApplicationContext.getBeanByClassOrName(clazz: Class<out Any>): Any {
+    try {
+        return this.getBean(clazz)
+    } catch (_: Exception) {}
+
+    try {
+        return this.getBean(StringUtils.uncapitalize(clazz.simpleName))
+    } catch (_: Exception) {}
+
+    val name = when {
+        clazz.isAnnotationPresent(Repository::class.java) -> clazz.getAnnotation(Repository::class.java).value
+        clazz.isAnnotationPresent(Service::class.java) -> clazz.getAnnotation(Service::class.java).value
+        clazz.isAnnotationPresent(Component::class.java) -> clazz.getAnnotation(Component::class.java).value
+        else -> throw RuntimeException("The bean $clazz was not found")
+    }
+    try {
+        return this.getBean(name)
+    } catch (_: Exception) {
+        throw RuntimeException("The bean $clazz was not found")
+    }
+}
 
 /**
  * This method get a bean and execute a method on it using the ApplicationContext
@@ -11,7 +37,7 @@ import java.util.*
  * @see org.springframework.context.ApplicationContext
  */
 fun ApplicationContext.getBeanAndExecute(repository: Class<out Any>, method: String, value: Any): Any? {
-    val instance = this.getBeanProvider(repository)
+    val instance = this.getBeanByClassOrName(repository)
     val callable = getMethodIfAvailable(repository, method, value::class.java)
         ?: getMethod(repository, method)
     return callable.invoke(instance, value)
