9f8da5d7778a68ca2542c0be0c3fd298b02be4bc
Giorgio-work <giorgio.work24@gmail.com>
2025-05-07 23:02:13 +0200

    feat: new annotations NullOrNotBlank and NullOrNotEmpty created

---
+++ src/test/kotlin/io/github/robertomike/jakidate/validations/strings/nullable/NullOrNotBlankTest.kt
@@ -0,0 +1,27 @@
+package io.github.robertomike.jakidate.validations.strings.nullable
+
+import io.github.robertomike.jakidate.utils.YamlFileSource
+import io.github.robertomike.jakidate.validations.YamlTest
+import jakarta.validation.Validator
+import org.junit.jupiter.api.Test
+
+@YamlFileSource("/strings/nullable/nullOrNotBlank")
+class NullOrNotBlankTest : YamlTest() {
+    inner class Example(
+        @field:NullOrNotBlank
+        val value: String? = null
+    )
+
+    override fun getExample(value: String): Any {
+        return Example(value)
+    }
+
+    @Test
+    fun getExampleWithoutValue(validator: Validator) {
+        val example = Example()
+
+        val result = validator.validate(example)
+
+        assert(result.isEmpty())
+    }
+}
